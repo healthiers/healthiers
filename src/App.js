58b08d0e5b3a7333a96367ff101aa30d19b3ad4c
Ifeoluwa Sanni <47613365+pheobeayo@users.noreply.github.com>
2023-07-17 10:31:44 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Signup from "./pages/sign-up/Signup";
+import Signin from "./pages/sign-in/Signin";
+import Home from "./pages/home/Home";
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route  path="/" element={<Home />} />
+      <Route  path="/sign-up" element={<Signup />} />
+      <Route  path="/sign-in" element={<Signin />} />
+     </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
